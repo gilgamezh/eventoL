631ce44b3f860bf3712df4efd9b4fe3362fc1b47
gilgamezh <mail@gilgamezh.me>
2018-10-10 09:44:27 -0300

    properly check Activity status

--- eventol/manager/security.py
+++ eventol/manager/security.py
@@ -169,7 +169,7 @@ def is_activity_public():
             event_slug = kwargs['event_slug']
 
             if any([
-                    activity.get_status_display() == "Accepted",
+                    activity.status == "2",  # Accepted
                     not settings.PRIVATE_ACTIVITIES,
                     activity.owner.user == user,
                     user.is_authenticated() and is_reviewer(user, event_slug=event_slug)
